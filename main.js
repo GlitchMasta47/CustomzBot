b790d5c78b6885ed91d8e5bda2a1161694068317
GlitchMasta47 <glitchmasta47@hotmail.com>
2017-03-14 14:09:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 var express = require('express')
 var app = express()
-var $PORT = 80
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen($PORT, function () {
-  console.log('Example app listening on port ' + $PORT + '!')
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
