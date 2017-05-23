2f4ceb76823ec0379ade6bb735af1f1b7a6873a6
GlitchMasta47 <glitchmasta47@hotmail.com>
2017-05-23 15:38:56 -0500

    Update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT
 
-app.use(app.static('webdocs'))
+app.use(express.static('webdocs'))
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
