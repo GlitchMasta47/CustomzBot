8deb144b0628cb680cc75475e8391d6c0a42e874
GlitchMasta47 <glitchmasta47@hotmail.com>
2018-03-12 18:18:39 -0500

    Add easter egg & comments

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+// Variables //
+
 var express = require('express')
 var app = express()
-var port = process.env.PORT
+var port = process.env.PORT || 8080;
+
+// Register all files in the /webdocs/ folder for use in the web server //
 
 app.use(express.static('webdocs'))
+
+// Easter eggs //
+
+app.get('/secret', function (req, res) => {
+  res.end("Don't even try")
+})
+
+// Server setup, grabs the port and starts using it //
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
